f660b3dd5a2d308034a1f13e0dd10cc960537f15
justy87 <94300125@comunidadort.edu.ar>
2026-06-16 16:50:55 -0300

    router: rutas REST para duenos

---
+++ src/router/duenos.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import Controlador from '../controlador/duenos.js'
+
+class Router {
+  #controlador = null
+
+  constructor() {
+    this.#controlador = new Controlador()
+  }
+
+  config() {
+    const router = express.Router()
+
+    router.get('/', this.#controlador.obtenerDuenos)
+    router.get('/:id', this.#controlador.obtenerDuenos)
+    router.post('/', this.#controlador.guardarDueno)
+    router.put('/:id', this.#controlador.actualizarDueno)
+    router.delete('/:id', this.#controlador.borrarDueno)
+
+    return router
+  }
+}
+
+export default Router
