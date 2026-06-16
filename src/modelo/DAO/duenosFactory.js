6c33a2d7e5d3835f4fed32a020b90e35db57f3b7
justy87 <94300125@comunidadort.edu.ar>
2026-06-16 16:38:47 -0300

    factory: patron factory para duenos

---
+++ src/modelo/DAO/duenosFactory.js
@@ -0,0 +1,17 @@
+import ModeloMongoDB from './duenosMongoDB.js'
+
+class DuenosFactory {
+  static get(tipo) {
+    switch (tipo) {
+      case 'MONGODB':
+        console.log('**** Duenos: Persistiendo en MongoDB ****')
+        return new ModeloMongoDB()
+
+      default:
+        console.log('**** Duenos: Persistiendo en MongoDB por defecto ****')
+        return new ModeloMongoDB()
+    }
+  }
+}
+
+export default DuenosFactory
