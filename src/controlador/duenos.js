775a0f696957136115c4f60f081a00aa181eab10
justy87 <94300125@comunidadort.edu.ar>
2026-06-16 16:47:18 -0300

    controlador: endpoints HTTP para duenos

---
+++ src/controlador/duenos.js
@@ -0,0 +1,78 @@
+import Servicio from '../servicio/duenos.js'
+
+class Controlador {
+  #servicio = null
+
+  constructor() {
+    this.#servicio = new Servicio()
+  }
+
+  obtenerDuenos = async (req, res) => {
+    try {
+      const { id } = req.params
+      const duenos = await this.#servicio.obtenerDuenos(id)
+
+      res.json(duenos)
+    } catch (error) {
+      res.status(500).json({
+        url: req.url,
+        method: req.method,
+        error: error.message
+      })
+    }
+  }
+
+  guardarDueno = async (req, res) => {
+    try {
+      const datos = req.body
+
+      if (Object.keys(datos).length === 0) {
+        throw new Error('El cuerpo esta vacio')
+      }
+
+      const dueno = await this.#servicio.guardarDueno(datos)
+
+      res.json(dueno)
+    } catch (error) {
+      res.status(500).json({
+        url: req.url,
+        method: req.method,
+        error: error.message
+      })
+    }
+  }
+
+  actualizarDueno = async (req, res) => {
+    try {
+      const { id } = req.params
+      const datos = req.body
+
+      const dueno = await this.#servicio.actualizarDueno(id, datos)
+
+      res.json(dueno)
+    } catch (error) {
+      res.status(500).json({
+        url: req.url,
+        method: req.method,
+        error: error.message
+      })
+    }
+  }
+
+  borrarDueno = async (req, res) => {
+    try {
+      const { id } = req.params
+      const dueno = await this.#servicio.borrarDueno(id)
+
+      res.json(dueno)
+    } catch (error) {
+      res.status(500).json({
+        url: req.url,
+        method: req.method,
+        error: error.message
+      })
+    }
+  }
+}
+
+export default Controlador
